d11e227c07fdcc185340dce48230380f02a8ac06
Boris Valero <bvalero@framasoft.org>
2025-10-02 10:50:48 +0200

    fix: run php-cs-fixer to fix errors

--- lib/Controller/PageAdminController.php
+++ lib/Controller/PageAdminController.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace OCA\FramaSpace\Controller;
 
-use OCA\FramaSpace\AppInfo\Application;
 use OCP\AppFramework\Controller;
 use OCP\AppFramework\Http\Attribute\FrontpageRoute;
 use OCP\AppFramework\Http\Attribute\NoAdminRequired;
@@ -16,11 +15,11 @@ use OCP\AppFramework\Http\RedirectResponse;
  * @psalm-suppress UnusedClass
  */
 class PageAdminController extends Controller {
-    #[NoCSRFRequired]
-    #[NoAdminRequired]
-    #[OpenAPI(OpenAPI::SCOPE_IGNORE)]
-    #[FrontpageRoute(verb: 'GET', url: '/admin')]
-    public function index(): RedirectResponse {
-        return new RedirectResponse('https://forum.frama.space/t/premiers-pas-pour-les-personnes-administratrices-dun-espace-frama-space/74');
-    }
+	#[NoCSRFRequired]
+	#[NoAdminRequired]
+	#[OpenAPI(OpenAPI::SCOPE_IGNORE)]
+	#[FrontpageRoute(verb: 'GET', url: '/admin')]
+	public function index(): RedirectResponse {
+		return new RedirectResponse('https://forum.frama.space/t/premiers-pas-pour-les-personnes-administratrices-dun-espace-frama-space/74');
+	}
 }
--- lib/Controller/PageController.php
+++ lib/Controller/PageController.php
@@ -4,24 +4,22 @@ declare(strict_types=1);
 
 namespace OCA\FramaSpace\Controller;
 
-use OCA\FramaSpace\AppInfo\Application;
 use OCP\AppFramework\Controller;
 use OCP\AppFramework\Http\Attribute\FrontpageRoute;
 use OCP\AppFramework\Http\Attribute\NoAdminRequired;
 use OCP\AppFramework\Http\Attribute\NoCSRFRequired;
 use OCP\AppFramework\Http\Attribute\OpenAPI;
 use OCP\AppFramework\Http\RedirectResponse;
-use OCP\AppFramework\Http\TemplateResponse;
 
 /**
  * @psalm-suppress UnusedClass
  */
 class PageController extends Controller {
-    #[NoCSRFRequired]
-    #[NoAdminRequired]
-    #[OpenAPI(OpenAPI::SCOPE_IGNORE)]
-    #[FrontpageRoute(verb: 'GET', url: '/')]
-    public function index(): RedirectResponse {
-        return new RedirectResponse('https://forum.frama.space/t/centre-de-ressources/71');
-    }
+	#[NoCSRFRequired]
+	#[NoAdminRequired]
+	#[OpenAPI(OpenAPI::SCOPE_IGNORE)]
+	#[FrontpageRoute(verb: 'GET', url: '/')]
+	public function index(): RedirectResponse {
+		return new RedirectResponse('https://forum.frama.space/t/centre-de-ressources/71');
+	}
 }
