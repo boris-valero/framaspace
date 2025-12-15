a82c533f22f0848b6fce1b35bc9f31765c78ae2a
Boris Valero <bvalero@framasoft.org>
2025-12-15 17:47:40 +0100

    refactor: extract inline CSS to external admin-form.css file

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default createAppConfig(
   {
     main: resolve(join("src", "main.js")),
     "admin-settings": resolve(join("src", "admin-settings.js")),
+    "admin-form": resolve(join("src", "admin-form.css")),
   },
   {
     createEmptyCSSEntryPoints: true,
