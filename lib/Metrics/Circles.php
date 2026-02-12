9f02f055794568a8903d09a35c536cb524c5976b
Ordinateur <bvalero@framasoft.org>
2026-02-12 16:23:40 +0100

    feat: add Circles Metric

---
+++ lib/Metrics/Circles.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OCA\FramaSpace\Metrics;
+
+use OCP\IDBConnection;
+
+class Circles
+{
+    public function __construct(
+        private IDBConnection $db,
+    ) {}
+
+    public function circlesCount(): int
+    {
+        $qb = $this->db->getQueryBuilder();
+        $qb->selectAlias($qb->createFunction('COUNT(*)'), 'circle_count')
+            ->from('circles_circle');
+        $result = $qb->executeQuery();
+        $row = $result->fetch();
+        $result->closeCursor();
+        return (int)$row['circle_count'];
+    }
+
+    public function getMetrics(): array
+    {
+        return [
+            'Number of circles' => $this->circlesCount()
+        ];
+    }
+}
