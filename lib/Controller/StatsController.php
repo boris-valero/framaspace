c3a763fef6d9e52019a21c56e03f007a23c5319b
Ordinateur <bvalero@framasoft.org>
2026-02-12 16:23:17 +0100

    feat: Add Circles metrics to stats endpoint

--- lib/Controller/StatsController.php
+++ lib/Controller/StatsController.php
@@ -6,6 +6,7 @@ use OCA\FramaSpace\Metrics\Deck;
 use OCA\FramaSpace\Metrics\Tables;
 use OCA\FramaSpace\Metrics\Forms;
 use OCA\FramaSpace\Metrics\Collectives;
+use OCA\FramaSpace\Metrics\Circles;
 use OCP\AppFramework\Http\DataResponse;
 use OCP\AppFramework\OCSController;
 use OCP\IRequest;
@@ -20,6 +21,7 @@ class StatsController extends OCSController
 		private Tables $tables,
 		private Forms $forms,
 		private Collectives $collectives,
+		private Circles $circles,
 	) {
 		parent::__construct($appName, $request);
 	}
@@ -35,7 +37,8 @@ class StatsController extends OCSController
 			'deck' => $this->deck->getMetrics(),
 			'tables' => $this->tables->getMetrics(),
 			'forms' => $this->forms->getMetrics(),
-			'collectives' => $this->collectives->getMetrics()
+			'collectives' => $this->collectives->getMetrics(),
+			'circles' => $this->circles->getMetrics()
 		]);
 	}
 }
