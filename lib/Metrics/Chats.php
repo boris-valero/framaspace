9ce8285f592dfb8a49ee0f18c3c78130de3c7690
Ordinateur <bvalero@framasoft.org>
2026-02-16 17:34:56 +0100

    feat: add conversations metric

---
+++ lib/Metrics/Chats.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OCA\FramaSpace\Metrics;
+
+use OCP\IDBConnection;
+
+class Chats
+{
+    public function __construct(
+        private IDBConnection $db,
+    ) {}
+
+    public function countChats(): int
+    {
+        $qb = $this->db->getQueryBuilder();
+        $qb->selectAlias($qb->createFunction('COUNT(*)'), 'chat_count')
+            ->from('comments')
+            ->where($qb->expr()->eq('object_type', $qb->createNamedParameter('chat')));
+        $result = $qb->executeQuery();
+        $row = $result->fetch();
+        $result->closeCursor();
+        return (int)$row['chat_count'];
+    }
+
+    public function getMetrics(): array
+    {
+        return [
+            'Number of Messages' => $this->countChats()
+        ];
+    }
+}
