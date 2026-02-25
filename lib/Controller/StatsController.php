647f235d39f4a041438529e09275bd1a65c22be8
Ordinateur <bvalero@framasoft.org>
2026-02-25 17:39:15 +0100

    perf: implement cache for stats response for 6 hours

--- lib/Controller/StatsController.php
+++ lib/Controller/StatsController.php
@@ -16,6 +16,7 @@ use OCP\AppFramework\Http\Attribute\CORS;
 use OCP\AppFramework\Http\Attribute\NoCSRFRequired;
 use OCP\AppFramework\Http\DataResponse;
 use OCP\AppFramework\OCSController;
+use OCP\ICacheFactory;
 use OCP\IRequest;
 
 /**
@@ -23,10 +24,14 @@ use OCP\IRequest;
  */
 
 class StatsController extends OCSController {
+	private const CACHE_TTL_SECONDS = 6 * 3600;
+	private const CACHE_NAMESPACE = 'framaspace-stats';
+	private const CACHE_KEY = 'all-metrics-v1';
 
 	public function __construct(
 		string $appName,
 		IRequest $request,
+		private ICacheFactory $cacheFactory,
 		private Deck $deck,
 		private Tables $tables,
 		private Forms $forms,
@@ -43,13 +48,21 @@ class StatsController extends OCSController {
 	/**
 	 * Get statistics for all apps
 	 *
-	 * @return DataResponse<200, array{deck: array, tables: array, forms: array, collectives: array, circles: array, calendars: array, talk: array, filecache: array}, array{}>
+	 * @return DataResponse<200, array, array{}>
 	 */
 	#[NoCSRFRequired]
 	#[CORS]
 	#[ApiRoute(verb: 'GET', url: '/api/v1/stats')]
 	public function getStats(): DataResponse {
-		return new DataResponse([
+		$cache = $this->cacheFactory->createLocal(self::CACHE_NAMESPACE);
+		/** @var mixed $cachedMetrics */
+		$cachedMetrics = $cache->get(self::CACHE_KEY);
+
+		if (is_array($cachedMetrics)) {
+			return new DataResponse($cachedMetrics);
+		}
+
+		$metrics = [
 			'deck' => $this->deck->getMetrics(),
 			'tables' => $this->tables->getMetrics(),
 			'forms' => $this->forms->getMetrics(),
@@ -61,6 +74,10 @@ class StatsController extends OCSController {
 				$this->chats->getMetrics()
 			),
 			'filecache' => $this->filecache->getMetrics()
-		]);
+		];
+
+		$cache->set(self::CACHE_KEY, $metrics, self::CACHE_TTL_SECONDS);
+
+		return new DataResponse($metrics);
 	}
 }
