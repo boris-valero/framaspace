4097f905d34236e47aeba58241ffaa59149e874f
Ordinateur <bvalero@framasoft.org>
2026-03-04 13:37:51 +0100

    refactor: harmonize metrics method naming convention

--- lib/Metrics/Collectives.php
+++ lib/Metrics/Collectives.php
@@ -8,18 +8,18 @@ namespace OCA\FramaSpace\Metrics;
  * @psalm-suppress PossiblyUnusedMethod, MixedAssignment, MixedArrayAccess
  */
 class Collectives extends BaseMetrics {
-	public function collectivesNumber(): int {
+	public function countCollectives(): int {
 		return $this->executeCount('collectives', 'collectives_number');
 	}
 
-	public function collectivesPages(): int {
+	public function countPages(): int {
 		return $this->executeCount('collectives_pages', 'page_count');
 	}
 
 	public function getMetrics(): array {
 		return [
-			'collectives' => $this->collectivesNumber(),
-			'pages' => $this->collectivesPages()
+			'collectives' => $this->countCollectives(),
+			'pages' => $this->countPages()
 		];
 	}
 }
