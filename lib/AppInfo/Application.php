a94bf8d6b54a1eecedceea4782598d8b465abed0
Ordinateur <bvalero@framasoft.org>
2026-03-05 17:27:21 +0100

    refactor: Remove unnecessary service registration

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace OCA\FramaSpace\AppInfo;
 
 use OCA\FramaSpace\Listener\CSSInjectionListener;
-use OCA\FramaSpace\Service\ConfigProxy; // <-- ton service config custom
 use OCP\AppFramework\App;
 use OCP\AppFramework\Bootstrap\IBootContext;
 use OCP\AppFramework\Bootstrap\IBootstrap;
@@ -24,14 +23,6 @@ class Application extends App implements IBootstrap {
 
 	public function register(IRegistrationContext $context): void {
 		$context->registerEventListener(BeforeTemplateRenderedEvent::class, CSSInjectionListener::class);
-
-		// Enregistrement explicite du service ConfigProxy si nécessaire
-		$context->registerService(ConfigProxy::class, function ($c) {
-			// Injection du service de config natif de Nextcloud si besoin
-			/** @var \OCP\IAppConfig $appConfig */
-			$appConfig = $c->get(\OCP\IAppConfig::class);
-			return new ConfigProxy($appConfig);
-		});
 	}
 
 	public function boot(IBootContext $context): void {
