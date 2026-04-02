a02e75efb1a4640e0fea65135f259ce9053b65e7
Ordinateur <bvalero@framasoft.org>
2026-04-02 18:51:21 +0200

    feat: add intermediate help routes for frontpage redirects

--- lib/Controller/PageController.php
+++ lib/Controller/PageController.php
@@ -9,19 +9,47 @@ use OCP\AppFramework\Http\Attribute\FrontpageRoute;
 use OCP\AppFramework\Http\Attribute\NoCSRFRequired;
 use OCP\AppFramework\Http\Attribute\OpenAPI;
 use OCP\AppFramework\Http\RedirectResponse;
+use OCP\IRequest;
+use OCP\IURLGenerator;
 
 class PageController extends Controller {
-	#[NoCSRFRequired]
-	#[OpenAPI(OpenAPI::SCOPE_IGNORE)]
-	#[FrontpageRoute(verb: 'GET', url: '/')]
-	public function index(): RedirectResponse {
-		return new RedirectResponse('https://forum.frama.space/t/centre-de-ressources/71');
-	}
+    public function __construct(
+        string $appName,
+        IRequest $request,
+        private IURLGenerator $urlGenerator,
+    ) {
+        parent::__construct($appName, $request);
+    }
 
-	#[NoCSRFRequired]
-	#[OpenAPI(OpenAPI::SCOPE_IGNORE)]
-	#[FrontpageRoute(verb: 'GET', url: '/admin')]
-	public function admin(): RedirectResponse {
-		return new RedirectResponse('https://forum.frama.space/t/premiers-pas-pour-les-personnes-administratrices-dun-espace-frama-space/74');
-	}
+    #[NoCSRFRequired]
+    #[OpenAPI(OpenAPI::SCOPE_IGNORE)]
+    #[FrontpageRoute(verb: 'GET', url: '/')]
+    public function index(): RedirectResponse {
+        return new RedirectResponse(
+            $this->urlGenerator->linkToRouteAbsolute('framaspace.page.help')
+        );
+    }
+
+    #[NoCSRFRequired]
+    #[OpenAPI(OpenAPI::SCOPE_IGNORE)]
+    #[FrontpageRoute(verb: 'GET', url: '/help')]
+    public function help(): RedirectResponse {
+        return new RedirectResponse('https://forum.frama.space/t/centre-de-ressources/71');
+    }
+
+    #[NoCSRFRequired]
+    #[OpenAPI(OpenAPI::SCOPE_IGNORE)]
+    #[FrontpageRoute(verb: 'GET', url: '/admin')]
+    public function admin(): RedirectResponse {
+        return new RedirectResponse(
+            $this->urlGenerator->linkToRouteAbsolute('framaspace.page.helpAdmin')
+        );
+    }
+
+    #[NoCSRFRequired]
+    #[OpenAPI(OpenAPI::SCOPE_IGNORE)]
+    #[FrontpageRoute(verb: 'GET', url: '/help/admin')]
+    public function helpAdmin(): RedirectResponse {
+        return new RedirectResponse('https://forum.frama.space/t/premiers-pas-pour-les-personnes-administratrices-dun-espace-frama-space/74');
+    }
 }
